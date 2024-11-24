df34879cd980fd54533afde1fa6f3f1b5d350744
SatyaKapardi <94280881+SatyaKapardi@users.noreply.github.com>
2024-11-23 19:58:09 -0600

    Updates

---
+++ precache-manifest.8f98accb34db59636e059606bd552abd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71238f01db089589d6517762333cf99c",
+    "url": "/Ehealth_frontend/index.html"
+  },
+  {
+    "revision": "18bfcfaf68beb1a58e9e",
+    "url": "/Ehealth_frontend/static/css/main.1f5a70fc.chunk.css"
+  },
+  {
+    "revision": "976d797315b44bb51dc9",
+    "url": "/Ehealth_frontend/static/js/2.731903cc.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/Ehealth_frontend/static/js/2.731903cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bfcfaf68beb1a58e9e",
+    "url": "/Ehealth_frontend/static/js/main.a8a07955.chunk.js"
+  },
+  {
+    "revision": "15eb45075bbb9fbef277",
+    "url": "/Ehealth_frontend/static/js/runtime-main.6a61a46e.js"
+  }
+]);
